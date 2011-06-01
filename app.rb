243b4334a0d254831f751ab65b5b8822f9adf43d
User <user@ubuntu.(none)>
2011-06-01 09:12:25 +0100

    CG: Working pstore example with basic string

--- app.rb
+++ app.rb
@@ -1,18 +1,12 @@
 require 'rubygems'
+require 'bundler/setup'
 require 'haml'
 require 'sinatra'
-#require 'pstore'
 
 class MyPetProject < Sinatra::Base
 
-  configure do
-#    @Test = 'test'
-#    @events = Events.new
-    set :public, "#{Dir.pwd}/public"
-  end
-
   get '/' do
-    haml :index
+      haml :index
   end
 
   get '/timeline' do
@@ -27,59 +21,30 @@ class MyPetProject < Sinatra::Base
     "Year is  #{year}"
   #  haml :event
   end
-#
-#  get '/event/:year/:month' do |year, month|
-#    if month > 12.to_s
-#      "There are only twelve months"
-#    else
-#      "Year is  #{year} "+
-#      "month is  #{month}"
-#    end
-#  #  haml :event
-#  end
-#
-#  get '/event/:year/:month/:day' do |year, month, day|
-#    @Test
-#  #  @events.events.each do |event|
-#  #    event
-#  #  end
-#  #
-#  ##  haml :event
-#  end
-#
-#  put '/event/:year/:month/:day' do |year, month, day|
-#  #  @events.add_event(:test1 => params["Test Page body for #{year}#{month}#{day}"])
-#  end
 
+  get '/event/:year/:month' do |year, month|
+    if month > 12.to_s
+      "There are only twelve months"
+    else
+      "Year is  #{year} "+
+      "month is  #{month}"
+    end
+  #  haml :event
+  end
+
+  get '/event/:year/:month/:day' do |year, month, day|
+    @@events.events.each do |event|
+      event
+    end
+  #  haml :event
+  end
+
+  put '/event/:year/:month/:day' do |year, month, day|
+    @@events.add_event 'cheese'
+#    @events.add_event(:test1 => params["Test Page body for #{year}#{month}#{day}"])
+    "Received #{year}#{month}#{day}"
+  end
+  run! if app_file == $0
 end
-
-#class Events
-#  def initialize(filename="events.pstore")
-#    @filename = filename
-#  end
-#
-#  def store
-#    @store ||= PStore.new(@filename)
-#  end
-#
-#  def add_event(event)
-#    store.transaction do
-#      store[:events] ||= []
-#      store[:events] << event
-#    end
-#  end
-#
-#  def events(readonly=true)
-#    store.transaction do
-#      store[:events]
-#    end
-#  end
-#
-#  def clear_events
-#    store.transaction do
-#      store[:events]=[]
-#    end
-#  end
-#end
 
 
--- config.ru
+++ config.ru
@@ -1,2 +1,42 @@
 require 'app'
+require 'pstore'
+
+class MyPetProject::Events
+  def initialize(filename="events1.pstore")
+    @filename = filename
+  end
+
+  def store
+    @store ||= PStore.new(@filename)
+  end
+
+  def add_event(event)
+    store.transaction do
+      store[:events] ||= []
+      store[:events] << event
+    end
+  end
+
+  def events(readonly=true)
+    store.transaction do
+      store[:events]
+    end
+  end
+
+  def clear_events
+    store.transaction do
+      store[:events]=[]
+    end
+  end
+end
+
+class MyPetProject < Sinatra::Base
+
+  configure do
+    @@events = MyPetProject::Events.new
+    set :public, "#{Dir.pwd}/public"
+  end
+
+end
+
 run MyPetProject
